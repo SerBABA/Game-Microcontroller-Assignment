a2634d369f93c68acd82cddc432f11269581f4b2
Dan Ronen <dro80@cs19098bs.canterbury.ac.nz>
2020-10-02 15:20:31 +1300

    Update 0.02 (does it work?)

--- Makefile
+++ Makefile
@@ -19,20 +19,54 @@ all: game.out
 game.o: game.c ../../drivers/avr/system.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
+ir_uart.o: ../../drivers/avr/ir_uart.c ../../drivers/avr/ir_uart.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h ../../drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+pio.o: ../../drivers/avr/pio.c ../../drivers/avr/pio.h ../../drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+prescale.o: ../../drivers/avr/prescale.c ../../drivers/avr/prescale.h ../../drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
 system.o: ../../drivers/avr/system.c ../../drivers/avr/system.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
+timer.o: ../../drivers/avr/timer.c ../../drivers/avr/system.h ../../drivers/avr/timer.h
+	$(CC) -c $(CFLAGS) $< -o $@
 
+timer0.o: ../../drivers/avr/timer0.c ../../drivers/avr/bits.h ../../drivers/avr/prescale.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+usart1.o: ../../drivers/avr/usart1.c ../../drivers/avr/system.h ../../drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+display.o: ../../drivers/display.c ../../drivers/avr/system.h ../../drivers/display.h ../../drivers/ledmat.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+ledmat.o: ../../drivers/ledmat.c ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/ledmat.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+navswitch.o: ../../drivers/navswitch.c ../../drivers/avr/delay.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/navswitch.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+font.o: ../../utils/font.c ../../drivers/avr/system.h ../../utils/font.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+pacer.o: ../../utils/pacer.c ../../drivers/avr/system.h ../../drivers/avr/timer.h ../../utils/pacer.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+tinygl.o: ../../utils/tinygl.c ../../drivers/avr/system.h ../../drivers/display.h ../../utils/font.h ../../utils/tinygl.h
+	$(CC) -c $(CFLAGS) $< -o $@
 
 # Link: create ELF output file from object files.
-game.out: game.o system.o
+game.out: game.o ir_uart.o pio.o prescale.o system.o timer.o timer0.o usart1.o display.o ledmat.o navswitch.o font.o pacer.o tinygl.o
 	$(CC) $(CFLAGS) $^ -o $@ -lm
 	$(SIZE) $@
 
 
 # Target: clean project.
 .PHONY: clean
-clean: 
+clean:
 	-$(DEL) *.o *.out *.hex
 
 
--- game.c
+++ game.c
@@ -1,26 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "system.h"
 #include "button.h"
 #include "pacer.h"
+#include "ir_uart.h"
+#include "navswitch.h"
 #include "tinygl.h"
-#include "../fonts/font3x5_1.h"
-#include <stdbool.h>
+#include "../fonts/font5x7_1.h"
 
 #define PACER_RATE 500
 #define MESSAGE_RATE 10
 #define TIMEOUT_LENGTH 5000
 
-bool ourWin(char ourChoice, char theirChoice) {
+uint8_t ourWin(char ourChoice, char theirChoice) {
 
     if (ourChoice == 'P' && theirChoice == 'R') {
-        return TRUE;
+        return 1;
     } else if (ourChoice == 'R' && theirChoice == 'S') {
-        return TRUE;
+        return 1;
     } else if (ourChoice == 'S' && theirChoice == 'P') {
-        return TRUE;
-    } else {
-        return FALSE;
+        return 1;
     }
+    return 0;
+}
 
+
+void display_character (char character)
+{
+    char buffer[2];
+    buffer[0] = character;
+    buffer[1] = '\0';
+    tinygl_text (buffer);
 }
 
 
@@ -31,14 +41,17 @@ int main (void)
     int ourChoice = 0;
     int i = 0;
     char theirChoice = 0;
-    bool waitChosenLetter = TRUE;
-    bool waitReceivedLetter = TRUE;
+    bool waitChosenLetter = 1;
+    bool waitReceivedLetter = 1;
 
     system_init ();
     tinygl_init (PACER_RATE);
     tinygl_font_set (&font5x7_1);
     tinygl_text_speed_set (MESSAGE_RATE);
     navswitch_init ();
+    ir_uart_init();
+
+    pacer_init (PACER_RATE);
 
 
     while (1) {
@@ -62,7 +75,7 @@ int main (void)
             // send letter // add error checks
             if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
                 ir_uart_putc(options[ourChoice]);
-                waitChosenLetter = FALSE;
+                waitChosenLetter = 0;
             }
         }
 
@@ -73,9 +86,9 @@ int main (void)
 
             if (ir_uart_read_ready_p()) {
                 theirChoice = ir_uart_getc();
-                for (i=0;i<amtChoice;i++) {
+                for (i=0;i<amtOptions;i++) {
                         if (theirChoice == options[i]) {
-                            waitReceivedLetter = FALSE;
+                            waitReceivedLetter = 0;
                         }
                 }
             }
@@ -87,14 +100,14 @@ int main (void)
         if (waitChosenLetter) {
             display_character(options[ourChoice]);
         } else if (waitReceivedLetter) {
-            display_character("X");
+            display_character('X');
         } else {
             if (options[ourChoice] == theirChoice) {
-                display_character("T");
+                display_character('T');
             } else if (ourWin(options[ourChoice], theirChoice)) {
-                display_character("W");
+                display_character('W');
             } else {
-                display_character("L");
+                display_character('L');
             }
             // waitReceivedLetter = TRUE;
             // waitChosenLetter = TRUE;
